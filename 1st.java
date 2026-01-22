e742e086003614382e6ca65c2403f34c1407eba3
Bablisingh100 <babli.singh_bca24@gla.ac.in>
2026-01-22 11:56:20 +0530

    Add files via upload

---
+++ 1st.java
@@ -0,0 +1,13 @@
+'''given an array count no. of element having atleast one element greater than itself'''
+class 1st{
+    public static void main(string[] args){
+        arr={-3,2,6,8,4,5,7}
+        count=0;
+        int max=0;
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i]
+            }
+        }
+    }
+}
